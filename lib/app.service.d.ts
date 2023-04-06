bd723abe06d0f7b47713fe5a9370d7673a70aa4a
Federico Interlandi Zoireff <ing.finterlandi@gmail.com>
2023-04-05 21:33:52 -0300

    cleaning dev

--- lib/app.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { DocumentData } from 'firebase-admin/firestore';
-import { FirebaseService } from './firebase/firebase.service';
-export declare class AppService {
-    private firebaseService;
-    constructor(firebaseService: FirebaseService);
-    getHello(): string;
-    getUsers(): Promise<DocumentData[]>;
-}
--- src/firebase/firebase.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { FirebaseService } from './firebase.service';
-import { UserFirebaseRepository } from './repositories/userFirebaseRepository';
-
-@Module({
-  providers: [FirebaseService, UserFirebaseRepository],
-  exports: [FirebaseService, UserFirebaseRepository],
-})
-export class FirebaseModule {}
